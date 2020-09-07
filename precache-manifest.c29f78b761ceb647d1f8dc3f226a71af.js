61784370c5604a62134da57e25f010091002d5c3
Gui <ggfl@cin.ufpe.br>
2020-09-07 12:04:50 -0300

    Updates

---
+++ precache-manifest.c29f78b761ceb647d1f8dc3f226a71af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f08ddc320826833241e4a79c56574b6",
+    "url": "/CS-roadmap/index.html"
+  },
+  {
+    "revision": "2dc22f432177d3c6fbd8",
+    "url": "/CS-roadmap/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "2686036a156468432ae2",
+    "url": "/CS-roadmap/static/css/main.60342f12.chunk.css"
+  },
+  {
+    "revision": "2dc22f432177d3c6fbd8",
+    "url": "/CS-roadmap/static/js/2.aa3fda90.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/CS-roadmap/static/js/2.aa3fda90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2686036a156468432ae2",
+    "url": "/CS-roadmap/static/js/main.c829b465.chunk.js"
+  },
+  {
+    "revision": "7b3abc9153049d549a9a",
+    "url": "/CS-roadmap/static/js/runtime-main.47c93301.js"
+  },
+  {
+    "revision": "cf2cf2f279d087203806ab134186b586",
+    "url": "/CS-roadmap/static/media/logo-fundo-colorido.cf2cf2f2.png"
+  }
+]);
